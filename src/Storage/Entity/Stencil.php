4e2387fad2ba26143bf8e0b60f20a9dd7b4f128f
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-20 11:51:23 +0100

    Entity

---
+++ src/Storage/Entity/Stencil.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bolt\Extension\AuthorName\Stencil\Storage\Entity;
+
+use Bolt\Storage\Entity\Entity;
+
+/**
+ * Stencil entity class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class Stencil extends Entity
+{
+    /** @var string */
+    protected $stencil;
+}

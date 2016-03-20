667cf5b7144bd3c1636d410f82b92be377c7c26f
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-20 11:49:35 +0100

    Extension loader class

---
+++ src/StencilExtension.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Bolt\Extension\YourName\Stencil;
+
+use Bolt\Events\ControllerEvents;
+use Bolt\Extension\AuthorName\Stencil\Provider\StencilServiceProvider;
+use Bolt\Extension\SimpleExtension;
+use Pimple as Container;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Stencil extension class.
+ *
+ * NOTE: All of these functions are safe to remove if not being used to modify
+ *       your extension's behaviour.
+ *
+ * @author Your Name <you@example.com>
+ */
+class StencilExtension extends SimpleExtension
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // Set up Functions
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerServices(Application $app)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function subscribe(EventDispatcherInterface $dispatcher)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getServiceProviders()
+    {
+        return [
+            $this,
+            new StencilServiceProvider($this->getConfig()),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(Application $app)
+    {
+        $this->container = $app;
+        $this->container['dispatcher']->addSubscriber($this);
+        $this->subscribe($this->container['dispatcher']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ControllerEvents::MOUNT => [
+                ['onMountRoutes', 0],
+                ['onMountControllers', 0],
+            ],
+        ];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Asset Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerAssets()
+    {
+        return [];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Configuration Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultConfig()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerFields()
+    {
+        return [];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Controller Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerFrontendControllers()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerBackendControllers()
+    {
+        return [];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Route Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerFrontendRoutes(ControllerCollection $collection)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerBackendRoutes(ControllerCollection $collection)
+    {
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Extension Database Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerExtensionTables()
+    {
+        $app = $this->getContainer();
+
+        return [
+            'stencil' => $app['stencil.schema.table']['stencil'],
+        ];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Extension Menu Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerMenuEntries()
+    {
+        return [];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Nut Command Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerNutCommands(Container $container)
+    {
+        return [];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Twig Functions.
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigFunctions()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigFilters()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigPaths()
+    {
+        return [];
+    }
+}
